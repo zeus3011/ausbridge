addb658e96fea8ae6efd747b3146972586321e6b
Aditya Jadon <adi.jadon7@gmail.com>
2026-05-05 16:30:38 +0530

    added env

--- src/sanityClient.ts
+++ src/sanityClient.ts
@@ -1,8 +1,8 @@
 import { createClient } from "@sanity/client";
 
 export const client = createClient({
-  projectId: "cptmwzqn",   // from sanity dashboard
-  dataset: "production",
-  apiVersion: "2024-01-01",
-  useCdn: true,
+  projectId: import.meta.env.VITE_SANITY_PROJECT_ID,
+  dataset: import.meta.env.VITE_SANITY_DATASET,
+  apiVersion: import.meta.env.VITE_SANITY_API_VERSION || "2024-01-01",
+  useCdn: String(import.meta.env.VITE_SANITY_USE_CDN).toLowerCase() === "true",
 });
